7798451e328944c7993429d4fa499e995409af6d
Vansh Gupta <you@example.com>
2023-07-21 09:52:45 +0530

    1st

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[user,setUser]=useState({name:"",password:""})
+  let[error,setError]=useState("");
+  function validator(e){
+    e.preventDefault();
+    if(user.name=="" || user.password==""){
+      setError("Both username and password are required")
+
+    }
+  }
   return (
+  
     <div>
-        {/* Do not remove the main div */}
+        <form onSubmit={validator}>
+        <label for="name">Username:</label>
+        <input 
+        type="text"
+        name="name"
+         onChange={(e)=>setUser(...user,user.name=e.target.value)}
+        />
+        <label for="password">Password:</label>
+        <input
+        type="password"
+        name="password"
+        onChange={(e)=>setUser(...user,user.password=e.target.value)}
+
+
+        />
+        <h5 id="errorMeassage">{error}</h5>
+        <button type="submit">Login</button>
+        </form>
     </div>
   )
 }
